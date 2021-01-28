1e9ecbee1d438e8518dc27e208c459eb06fc9aad
Yuki Ito <37338201+ykit00@users.noreply.github.com>
2021-01-28 23:31:18 +0900

    Install CoreUI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "bootstrap"
-import "popper.js"
+import "@coreui/coreui"
 
 import "../stylesheets/application"
 
